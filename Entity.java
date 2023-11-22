7a8bad2ce6c086c47d0d0698d58222f6c04ec297
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-11-22 13:41:50 +0700

    initial commit

---
+++ Entity.java
@@ -0,0 +1,9 @@
+package TicTacQuest;
+
+public class Entity {
+    String name;
+
+    Entity(String name) {
+        this.name = name;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+package TicTacQuest;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
